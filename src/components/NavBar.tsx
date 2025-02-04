4cd4139e26b30820e8d593bcf583133b0bd6a2b8
fgamester <falvarez.munoz@hotmail.es>
2025-02-04 19:39:33 -0300

    colors updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,9 @@ import DevtreeLogo from "./icons/DevtreeLogo";
 export default function NavBar() {
     return (
         <>
-            <nav className="bg-black/50 flex">
-
-                <Link to='/' className=" p-3 block w-fit">
-                    <DevtreeLogo className=" text-4xl" title="Devtree logo" titleId="devtree-logo"/>
+            <nav className="bg-[#024873] flex px-10">
+                <Link to='/' className="py-3 block w-fit">
+                    <DevtreeLogo className="text-4xl" title="Devtree logo" titleId="devtree-logo"/>
                 </Link>
             </nav>
         </>
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,11 +3,6 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <nav className="bg-black/50">
-        <Link to='/'>
-          <img src="/logo.svg" alt="devtree_logo" />
-        </Link>
-      </nav>
       <nav>
         <p>Don't you have an account yet? <Link to='/auth/register'>Sign Up</Link></p>
       </nav>
